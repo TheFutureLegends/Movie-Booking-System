1683539b17b916e6192f39ac91141a6c9e099f2f
quocthang276 <pyn276@gmail.com>
2020-08-26 17:09:42 +0700

    add dependency flyway, movie.html edited

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,7 +5,8 @@ create table movie_booking.user
     user_name varchar(500) null,
     password  varchar(500) null,
     role      varchar(500) null
-) ;
+) engine = innoDB charset = utf8 ;
+
 create table movie_booking.movie
 (
     id                   int auto_increment
@@ -22,5 +23,28 @@ create table movie_booking.movie
     description          varchar(2500) null,
     movie_start_time     datetime      null,
     movie_available_seat int           null
-);
+) engine = innoDB charset = utf8 ;
+
+create table movie_booking.user_profile
+(
+    id        int auto_increment
+        primary key,
+    full_name varchar(300) null,
+    phone     varchar(100) null,
+    email     varchar(100) null,
+    address   varchar(500) null
+) engine = innoDB charset = utf8 ;
+
+create table movie_booking.reservation
+(
+    id           int auto_increment
+        primary key,
+    name         varchar(200) null,
+    date_time    datetime     null,
+    status       varchar(100) null,
+    u_profile_id int          null,
+    constraint reservation_user_profile_id_fk
+        foreign key (u_profile_id) references movie_booking.user_profile (id)
+) engine = innoDB charset = utf8 ;
+
 
