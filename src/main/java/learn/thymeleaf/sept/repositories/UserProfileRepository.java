0801f3fdf55edbbc8c7bf939589b36fb3b730b56
quocthang276 <pyn276@gmail.com>
2020-08-28 14:13:07 +0700

    "make CRUD User Profile"

--- src/main/java/learn/thymeleaf/sept/repositories/UserProfileRepository.java
+++ src/main/java/learn/thymeleaf/sept/repositories/UserProfileRepository.java
@@ -2,7 +2,12 @@ package learn.thymeleaf.sept.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import learn.thymeleaf.sept.entity.UserProfile;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile,Integer> {
+    @Query(value = "SELECT * FROM user_profile up WHERE up.full_name LIKE %:keyword%", nativeQuery = true)
+    List<UserProfile> findByUserProfileName(String keyword);
     // CRUD User Profile DAO, no need more code for CRUD, no implementation class :)
 }
--- src/main/java/learn/thymeleaf/sept/repositories/UserRepository.java
+++ src/main/java/learn/thymeleaf/sept/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package learn.thymeleaf.sept.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import learn.thymeleaf.sept.entity.User;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+    @Query(value = "SELECT * FROM user u WHERE u.user_name LIKE %:keyword%", nativeQuery = true)
     List<User> findByUserName(String keyword);
     // CRUD User DAO, no need more code for CRUD, no implementation class :)
 }
